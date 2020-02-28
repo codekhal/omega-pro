b3027e8760f63ffd67e0d319e0ffa192d6485269
khushal <vyas.khushal1999@gmail.com>
2020-02-28 17:39:54 +0530

    adding more programmes

--- DS-Algo/Recursion/DigitalRoot.cpp
+++ DS-Algo/Recursion/DigitalRoot.cpp
@@ -1,9 +1,26 @@
 // DigitalRoot of a number is the recursive sum of its digits until we get a single digit number.
 // Eg: DigitalRoot(191)=1+9+1=>11=>1+1=>2
 
-// We can think it as calculating sum of digits of a number twice, Until it becomes less than 10.
+// Solution 1. We can think it as calculating sum of digits of a number twice, Until it becomes less than 10.
+int rec(int sum)
+{
+    if(sum/10 == 0)
+        return sum;
+    else
+        return (sum % 10 + rec(sum/10));
+}
+int digitalRoot(int n)
+{
+    
+    if(n/10 == 0)
+        return n;
 
-// Best Solution
+    else
+        return (digitalRoot(rec(n)));
+}
+
+
+// Solution 2. Best Solution
 int digitalRoot(int n)
 {
     if(n < 10)
---
+++ DS-Algo/Recursion/Fibonacci.cpp
@@ -0,0 +1,29 @@
+// Find nth fiboibonacci number:
+
+
+// Non-DP (Dynamic Programming) Solution 
+long long fibonacci(int n)
+{
+
+    if(n <= 1)
+        return n;
+        
+    return (fibonacci(n-1) + fibonacci(n-2));
+}
+
+// Using DP(Let's not be afraid of DP, in DP the only thing we do is store prefix sum which is used in future function calls. That's it!)
+
+long long fibonacci(int n)
+{
+    int fibo[n+2]; // Taking Size of static array with n+2
+
+    fibo[0]=0;
+    fibo[1]=1;
+
+    for(int i=2;i<=n;i++)
+    {
+        fibo[i]=fibo[i-1]+fibo[i-2]; // Storing sum so it could be reused for future computations.
+    }
+
+    return fibo[n];
+}
