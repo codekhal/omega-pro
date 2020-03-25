3eede7a8c58b806832f6a8a0fcdf4e3708d2c46a
khushal <vyas.khushal1999@gmail.com>
2020-03-25 09:52:24 +0530

    second largest element in SQL

--- SQL Interview/SecondLargest.sql
+++ SQL Interview/SecondLargest.sql
@@ -1,4 +1,4 @@
-/* To get the second highest salary from the database. */
+/* We have a table called emp, where we have details about employee. To get the second highest salary from the database. */
 
 select max(salary) from emp
 where salary not in (select max(salary) from emp);
